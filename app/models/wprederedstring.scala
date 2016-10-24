9c93972847cab76ae967dace5de055fea9a2da90
Dave Ross <dave@davidmichaelross.com>
2016-10-24 19:23:56 -0400

    WPRenderedString is not JSON serializable

--- app/models/wprederedstring.scala
+++ app/models/wprederedstring.scala
@@ -1,5 +1,18 @@
 package wpapi.models
 
+import play.api.libs.json._
+
 class WPRenderedString(pRedered:String) {
     var rendered = pRedered
 }
+
+object WPRenderedString {
+    implicit val wpRenderedStringWrites:Writes[WPRenderedString] = new Writes[WPRenderedString] {
+        def writes(c: WPRenderedString): JsValue = {
+            Json.obj(
+                "rendered" -> c.rendered
+            )
+        }
+    }
+}
+
