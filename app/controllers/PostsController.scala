7096c1832c680976d624ff5e974506eb71a0a72e
Dave Ross <dave@davidmichaelross.com>
2016-10-25 23:25:32 -0400

    Initial support for _jsonp query parameter

--- app/controllers/PostsController.scala
+++ app/controllers/PostsController.scala
@@ -6,11 +6,18 @@ import play.api.mvc._
 import play.api.libs.json._
 import wpapi.models._
 
-@Singleton
-class PostsController extends Controller {
+trait JSONPCompatibleController {
+    def toJson(json: JsValue, jsonp: Option[String]) : String = jsonp match {
+        case Some(f) => "/**/" + f + '(' + Json.stringify(json) + ')'
+        case None => Json.stringify(json)
+    }
+}
 
-    def query = Action {
+@Singleton
+class PostsController extends Controller with JSONPCompatibleController {
+
+    def query(_jsonp: Option[String]) = Action {
         var results:Array[WPPost] = Array(new WPPost, new WPPost, new WPPost, new WPPost)
-        Ok(Json.toJson(results))
+        Ok(toJson(Json.toJson(results), _jsonp))
     }
 }
